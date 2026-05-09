cefb19565ac391bb1d82475313f96e453b019ad0
leavul <albaraa.yadag@gmail.com>
2026-05-09 21:04:10 +0300

    chore(backend): add seed data for testing

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -7,3 +7,7 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+(0..5).each do |n|
+  BankAccount.create(account_number: SecureRandom.hex(6))
+end
